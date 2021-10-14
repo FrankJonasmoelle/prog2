0ac69eb79c88c6fb248d891c22b45ad6f92a8bab
Jonas <frankemoelle.jonas@gmail.com>
2021-10-14 09:43:40 +0200

    Added fibonacci time counter

--- MA4_files/MA4_2.py
+++ MA4_files/MA4_2.py
@@ -1,19 +1,45 @@
 #!/usr/bin/env python3
+from time import perf_counter as pc
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
 
 from integer import Integer
 
-def fib_py(n):
-	if n <= 1:
-		return n
-	else:
-		return(fib_py(n-1) + fib_py(n-2))
+def fib_pure_py(n):
+    if n <= 1:
+        return n
+    else:
+        return(fib_pure_py(n-1) + fib_pure_py(n-2))
+
 
 def main():
-	f = Integer(5)
-	print(f.get())
-	f.set(7)
-	print(f.get())
-	print(f.fib())
+    time_python = []
+    time_cpp = []
+    for i in range(30, 45):
+        # python
+        start = pc()
+        fib_pure_py(i)
+        end = pc()
+        time_python.append(round(end-start, 2))
+
+        # cpp
+        start = pc()
+        f = Integer(i)
+        f.fib()
+        end = pc()
+        time_cpp.append(round(end-start, 2))
+
+    plt.plot(x=range(30, 45), y=time_python)
+    plt.plot(x=range(30, 45), y=time_cpp)
+    plt.savefig("fibonacci_timing.png")
+
+
+    # f = Integer(5)
+    # print(f.get())
+    # f.set(7)
+    # print(f.get())
+    # print(f.fib())
 
 if __name__ == '__main__':
-	main()
+    main()
